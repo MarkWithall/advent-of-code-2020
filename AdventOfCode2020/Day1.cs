841f0700e862e709d832c53905ccaa2c36acc54f
Mark Withall <mark.withall@tracsis.com>
2020-12-01 08:00:30 +0000

    Initial project files.

---
+++ AdventOfCode2020/Day1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2020
+{
+    [TestFixture(TestName = "Day 1: Report Repair")]
+    public class Day1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace AdventOfCode2020
+{
+    internal static class Program
+    {
+        public static int Main(string[] args) => new NUnitLite.AutoRun().Execute(args);
+    }
+}
