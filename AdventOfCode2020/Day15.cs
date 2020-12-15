21030858bc1257cac69ebe45251b90ecb1a538ad
Mark Withall <mark.withall@tracsis.com>
2020-12-15 07:46:45 +0000

    Day 15, part 2.

--- AdventOfCode2020/Day15.cs
+++ AdventOfCode2020/Day15.cs
@@ -13,16 +13,26 @@ namespace AdventOfCode2020
             Assert.AreEqual(412, NthNumberSpoken("10,16,6,0,1,17", 2020));
         }
 
-        [TestCase("0,3,6", 436)]
-        [TestCase("1,3,2", 1)]
-        [TestCase("2,1,3", 10)]
-        [TestCase("1,2,3", 27)]
-        [TestCase("2,3,1", 78)]
-        [TestCase("3,2,1", 438)]
-        [TestCase("3,1,2", 1836)]
-        public void Part1Sample(string input, long expectedOutput)
+        [Test]
+        public void Part2()
         {
-            Assert.AreEqual(expectedOutput, NthNumberSpoken(input, 2020));
+            Assert.AreEqual(243, NthNumberSpoken("10,16,6,0,1,17", 30000000));
+        }
+
+        [TestCase("0,3,6", 436, 175594)]
+        [TestCase("1,3,2", 1, 2578)]
+        [TestCase("2,1,3", 10, 3544142)]
+        [TestCase("1,2,3", 27, 261214)]
+        [TestCase("2,3,1", 78, 6895259)]
+        [TestCase("3,2,1", 438, 18)]
+        [TestCase("3,1,2", 1836, 362)]
+        public void Part1Sample(string input, long expectedOutput1, long expectedOutput2)
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expectedOutput1, NthNumberSpoken(input, 2020));
+                Assert.AreEqual(expectedOutput2, NthNumberSpoken(input, 30000000));
+            });
         }
 
         private static long NthNumberSpoken(string input, long n)
