92c84b595da139bfd55c368593866ded19e0e30d
Mark Withall <mark.withall@tracsis.com>
2020-12-17 07:44:29 +0000

    Day 17, part 2.

--- AdventOfCode2020/Day17.cs
+++ AdventOfCode2020/Day17.cs
@@ -10,16 +10,28 @@ namespace AdventOfCode2020
         [Test]
         public void Part1()
         {
-            Assert.AreEqual(263, ActiveCubes(Day17Input, 6));
+            Assert.AreEqual(263, ActiveCubes3D(Day17Input, 6));
         }
-        
+
         [Test]
         public void Part1Sample()
         {
-            Assert.AreEqual(112, ActiveCubes(Day17SampleInput, 6));
+            Assert.AreEqual(112, ActiveCubes3D(Day17SampleInput, 6));
         }
 
-        private static long ActiveCubes(string[] input, long cycles)
+        [Test]
+        public void Part2()
+        {
+            Assert.AreEqual(1680, ActiveCubes4D(Day17Input, 6));
+        }
+
+        [Test]
+        public void Part2Sample()
+        {
+            Assert.AreEqual(848, ActiveCubes4D(Day17SampleInput, 6));
+        }
+
+        private static long ActiveCubes3D(string[] input, long cycles)
         {
             Dictionary<(long, long, long), bool> grid = new();
 
@@ -73,6 +85,66 @@ namespace AdventOfCode2020
                 for (var x = 0; x < input[y].Length; x++)
                 {
                     grid[(x, y, 0)] = input[x][y] == '#';
+                }
+            }
+        }
+
+        private static long ActiveCubes4D(string[] input, long cycles)
+        {
+            Dictionary<(long, long, long, long), bool> grid = new();
+
+            ReadInput();
+            for (var i = 0; i < cycles; i++)
+            {
+                grid = Simulate(grid);
+            }
+
+            return grid.Values.Count(v => v);
+
+            Dictionary<(long, long, long, long), bool> Simulate(Dictionary<(long x, long y, long z, long w), bool> initialGrid)
+            {
+                Dictionary<(long, long, long, long), bool> newGrid = new();
+
+                for (var x = initialGrid.Keys.Min(k => k.x) - 1; x <= initialGrid.Keys.Max(k => k.x) + 1; x++)
+                for (var y = initialGrid.Keys.Min(k => k.y) - 1; y <= initialGrid.Keys.Max(k => k.y) + 1; y++)
+                for (var z = initialGrid.Keys.Min(k => k.z) - 1; z <= initialGrid.Keys.Max(k => k.z) + 1; z++)
+                for (var w = initialGrid.Keys.Min(k => k.w) - 1; w <= initialGrid.Keys.Max(k => k.w) + 1; w++)
+                {
+                    var coord = (x, y, z, w);
+
+                    List<(long, long, long, long)> neighbours = new();
+                    for (var xn = -1; xn <= 1; xn++)
+                    for (var yn = -1; yn <= 1; yn++)
+                    for (var zn = -1; zn <= 1; zn++)
+                    for (var wn = -1; wn <= 1; wn++)
+                    {
+                        if (xn != 0 || yn != 0 || zn != 0 || wn != 0)
+                        {
+                            neighbours.Add((coord.x + xn, coord.y + yn, coord.z + zn, coord.w + wn));
+                        }
+                    }
+
+                    var activeNeighbourCount = neighbours.Count(IsActive);
+
+                    if (IsActive(coord) && (activeNeighbourCount == 2 || activeNeighbourCount == 3) ||
+                        !IsActive(coord) && activeNeighbourCount == 3)
+                    {
+                        SetState(coord, true);
+                    }
+                }
+
+                return newGrid;
+
+                bool IsActive((long, long, long, long) coords) => initialGrid.TryGetValue(coords, out var state) && state;
+                void SetState((long, long, long, long) coords, bool state) => newGrid[coords] = state;
+            }
+
+            void ReadInput()
+            {
+                for (var y = 0; y < input.Length; y++)
+                for (var x = 0; x < input[y].Length; x++)
+                {
+                    grid[(x, y, 0, 0)] = input[x][y] == '#';
                 }
             }
         }
