6319d1b6d101465b548a23edcf41d3c85d13189d
Mark Withall <mark.withall@tracsis.com>
2021-02-05 12:17:37 +0000

    Simplify runner.

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -1,7 +1,1 @@
-﻿namespace AdventOfCode2020
-{
-    internal static class Program
-    {
-        public static int Main(string[] args) => new NUnitLite.AutoRun().Execute(args);
-    }
-}
+﻿new NUnitLite.AutoRun().Execute(args);
